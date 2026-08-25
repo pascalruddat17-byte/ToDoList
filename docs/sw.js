e5c0c1a175aec0cff4b441ecb96af51f29557b87
Emolio <pascal.ruddat17@gmail.com>
2026-08-25 15:52:33 +0200

    Add homescreen widget views

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "pulse-tasks-v9";
+const cacheName = "pulse-tasks-v10";
 const files = ["./", "./index.html", "./manifest.json", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
